84c2fda867c3984b5eb982883b5c6b79b389afa6
Erwin Eko Wahyudi <erwin.eko@bukalapak.com>
2021-04-16 08:44:11 +0700

    remove license

--- Pemrograman 2/6 - Heap/Main.java
+++ Pemrograman 2/6 - Heap/Main.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author erwinekowahyudi
- */
-
 public class Main {
     public static void main(String[] args){
         int[] keys =  new int[]{78, 3, 9, 10, 23, 77, 34, 86, 90, 100, 20, 66, 94, 63, 97};
