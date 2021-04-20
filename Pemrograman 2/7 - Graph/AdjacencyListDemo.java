73beab60a4f4cfaa9a24325031ec31f0aaff109e
Erwin Eko Wahyudi <erwin.eko@bukalapak.com>
2021-04-21 05:12:42 +0700

    Add adjacency matrix demo

---
+++ Pemrograman 2/7 - Graph/AdjacencyListDemo.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class AdjacencyListDemo {
+	public static void main(String[] args) {
+		int numVertex = 6;
+
+		AdjacencyListGraph graph = new AdjacencyListGraph(numVertex);
+
+		graph.addEdge(1, 2);
+		graph.addEdge(1, 5);
+		graph.addEdge(2, 3);
+		graph.addEdge(2, 5);
+		graph.addEdge(3, 4);
+		graph.addEdge(4, 5);
+		graph.addEdge(4, 6);
+
+		graph.displayGraph();
+
+	}
+}
+
+class AdjacencyListGraph {
+	private int numVertex;
+	private int[][] AdjacencyMatrix;
+
+	public AdjacencyMatrixGraph(int numVertex) {
+		this.numVertex = numVertex;
+		AdjacencyMatrix = new int[numVertex+1][numVertex+1];
+	}
+
+	public void addEdge(int one, int two) {
+		this.AdjacencyMatrix[one][two] = 1;
+		this.AdjacencyMatrix[two][one] = 1;
+	}
+
+	public void displayGraph() {
+		for (int i=1 ; i<=this.numVertex ; i++) {
+			for (int j=1 ; j<=this.numVertex ; j++) {
+				System.out.print(this.AdjacencyMatrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
