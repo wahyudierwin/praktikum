725e45c7fd2bde99c34ee206e9f6614a2618f708
wahyudierwin <wahyudierwin@gmail.com>
2022-03-19 06:29:06 +0700

    [ASD] Update heap main class

--- Algoritma dan Struktur Data/5 - Heap/Main.java
+++ Algoritma dan Struktur Data/5 - Heap/Main.java
@@ -1,15 +1,16 @@
 public class Main {
     public static void main(String[] args){
         int[] keys =  new int[]{78, 3, 9, 10, 23, 77, 34, 86, 90, 100, 20, 66, 94, 63, 97};
-        Heap myheap = new Heap(keys.length);
-        
+        Heap heap = new Heap(keys.length);
+
         for (int i=0 ; i<keys.length ; i++){
-            myheap.insert(keys[i]);
+            heap.insert(keys[i]);
         }
-        
-        myheap.displayHeap();
-        
-        myheap.remove();
-        myheap.displayHeap();
+
+        heap.displayHeap();
+
+        heap.remove();
+
+        heap.displayHeap();
     }
 }
